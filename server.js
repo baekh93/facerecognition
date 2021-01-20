7e0873fffd8089ba9775e42add249dfc571bf9de
keonho <khbae>
2021-01-20 15:25:36 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT = 8888;
+
+//public 폴더 안에 있는 파일들을 static file 로 인식하게 한다.
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log('Server is running at:',PORT);
+});
+
